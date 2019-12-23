860657428df8ccdfd37bcd52f511b91f887ad554
Akra20m <akra200m@gmail.com>
2019-12-23 02:01:10 -0500

    Fix Deployment

---
+++ client/build/precache-manifest.923e5dbdfeb76df1014b87bed8f81c79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6647ccb88652a60a8d5f547dfc440df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f13c4c2ef4c8ff6e7ae",
+    "url": "/static/css/main.ff93b160.chunk.css"
+  },
+  {
+    "revision": "f0e98df05fc339c6b233",
+    "url": "/static/js/2.10c02696.chunk.js"
+  },
+  {
+    "revision": "1f13c4c2ef4c8ff6e7ae",
+    "url": "/static/js/main.53f2bb04.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "cca703153db4c918f6aae9d24a1973ea",
+    "url": "/static/media/background.cca70315.jpg"
+  }
+]);
