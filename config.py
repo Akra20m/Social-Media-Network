4fa5adc537d2d292c3506d0c7043db08ec5c85dc
Akra20m <akra200m@gmail.com>
2019-12-22 19:55:16 -0500

    Ready for deployment

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = 'access'
